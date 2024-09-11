356f1a8dec2871a094c03d7807b6ec13a062697d
lazyshout <lazyshout@gamesoultech.com>
2024-09-11 17:33:56 +0800

    fix logger

--- src/common/Logger.ts
+++ src/common/Logger.ts
@@ -4,17 +4,17 @@ function CreateLogger (name: string): winston.Logger {
     return winston.createLogger({
         defaultMeta: { name: name },
         level: process.env.LOG_LEVEL ?? 'info', // Do not use low level in production.
+        format: winston.format.errors({ stack: true }), // This must be the first format and before transport. Otherwise, the stack will be dropped.
         transports: [
             new winston.transports.Console({
                 format: winston.format.combine(
                     winston.format.timestamp(),
-                    winston.format.errors({ stack: true }),
-
                     process.env.LOG_FORMAT === 'pretty' ? winston.format.prettyPrint({ colorize: true }) : winston.format.json(),
                 ),
             }),
         ],
     });
 }
+
 
 export { CreateLogger };
