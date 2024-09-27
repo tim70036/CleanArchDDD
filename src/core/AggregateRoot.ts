0b92427515def5cff8b15b177c6b648601cb9118
lazyshout <lazyshout@gamesoultech.com>
2024-09-27 15:16:39 +0800

    typo

--- src/core/AggregateRoot.ts
+++ src/core/AggregateRoot.ts
@@ -5,7 +5,7 @@ abstract class AggregateRoot<T> extends Entity<T> {
     // During modification of aggregate, domain events will happen.
     // They're all buffered here, waiting to be published by event
     // publisher. (Probably after the aggregate is saved to repo).
-    // TOOD: If the aggregate has nested entity in it. How to allow
+    // TODO: If the aggregate has nested entity in it. How to allow
     // that nested entity to push event into this buffer? Maybe we can
     // let every nested entity have their own buffered events. Then, a
     // public function of aggregate will collect all domain events and
